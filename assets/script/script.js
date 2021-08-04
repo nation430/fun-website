1796e664ef854e54e8e941cf8b931103cc28db0d
nation430 <tonyenation430@gmail.com>
2021-08-04 22:06:02 +0100

    commited another section

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,3 +16,28 @@ function createBoxes(){
 }
 
 createBoxes()
+
+
+//moving text
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+const text = 'Hello Welcome To This Fun Websites...'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText() {
+    textEl.innerText = text.slice(0, idx)
+
+    idx++
+
+    if(idx > text.length) {
+        idx = 1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
